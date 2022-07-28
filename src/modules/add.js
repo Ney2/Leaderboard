e89b18571f4f4588cab074a2cc0cec3d8c9f4fbe
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-07-28 21:05:08 +0300

    Update index.js, add.js and list.js file in the  modules folder

--- src/modules/add.js
+++ src/modules/add.js
@@ -1,7 +1,16 @@
-import { scores } from './list.js';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/bs04D0MFCfRaXPNteJu6/scores';
+const addScoreToList = async (pname, value) => {
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: pname,
+      score: value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json());
+};
 
-export default function addScoreToList(pname, value) {
-  scores.push({ pname, value });
-  localStorage.setItem('scoreList', JSON.stringify(scores));
-  return { pname, value };
-}
+export default addScoreToList;
